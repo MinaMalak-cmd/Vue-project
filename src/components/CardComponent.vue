be740251a154239be826c4c3c1822d59517dabbd
Mina Malak <mina.malak@tactful.ai>
2022-04-03 20:43:53 +0200

    finished layout

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,56 +1,76 @@
 <template>
-  <b-container class="card">
-    <b-row>
-      <b-col>
-        <div>
-          <b-card
-            title
-            img-src="https://i.ytimg.com/vi/wTWY0n97vuE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAhM5y5G2EPQFmJLzBw1SB1hsBVhg"
-            img-alt="Image"
-            img-top
-            tag="article"
-            class="mb-2"
-          >
-            <b-card-text class="card--header">من الصفر إلي الاحتراف VueJS 2</b-card-text>
-            <hr />
-            <b-card-text class="card--desc">
-              <i class="fab fa-youtube card--desc--icon"></i>
-              إحصائيات اليوتيوب
-            </b-card-text>
-            <hr />
-            <ul class="card--list">
-              <li>
-                <i class="fas fa-bars"></i> 66 درس
-              </li>
-              <li>
-                <i class="far fa-clock"></i> 03:00:00
-              </li>
-              <li>
-                <i class="far fa-play-circle"></i> 16,000 مشاهدة
-              </li>
-              <li>
-                <i class="far fa-thumbs-up"></i> 650 إعجاب
-              </li>
-              <li>
-                <i class="far fa-comments"></i> 96 تعليق
-              </li>
-            </ul>
-             <b-button class="w-100" href="#" variant="secondary">شاهد الآن</b-button>
-          </b-card>
-        </div>
-      </b-col>
-      <b-col>2</b-col>
-      <b-col>3</b-col>
-    </b-row>
-  </b-container>
+    <span>
+    <b-alert show class="w-50 m-auto">{{heading}}</b-alert>
+    <b-container class="card my-5">
+        <b-row>
+        <b-col>
+            <div>
+            <b-card
+                :title="data.name"
+                :img-src="data.thumbnails"
+                img-alt="Image"
+                img-top
+                tag="article"
+                class="mb-2"
+            >
+                <!-- <b-card-text class="card--header">من الصفر إلي الاحتراف VueJS 2</b-card-text> -->
+                <hr />
+                <b-card-text class="card--desc">
+                <i class="fab fa-youtube card--desc--icon"></i>
+                إحصائيات اليوتيوب
+                </b-card-text>
+                <hr />
+                <ul class="card--list">
+                <li>
+                    <i class="fas fa-bars"></i> {{data.statistics.lessons}} درس
+                </li>
+                <li>
+                    <i class="far fa-clock"></i> {{data.statistics.duration}}
+                </li>
+                <li>
+                    <i class="far fa-play-circle"></i> {{data.statistics.viewCount}} مشاهدة
+                </li>
+                <li>
+                    <i class="far fa-thumbs-up"></i> {{data.statistics.likeCount}} إعجاب
+                </li>
+                <li>
+                    <i class="far fa-comments"></i> {{data.statistics.commentCount}} تعليق
+                </li>
+                </ul>
+                <b-link class="w-100 btn btn-primary " :href="data.url" active-class target="blank">شاهد الآن</b-link>
+            </b-card>
+            </div>
+        </b-col>
+        <b-col>2</b-col>
+        <b-col>3</b-col>
+        </b-row>
+    </b-container>
+    </span>
 </template>
-
 <script lang="ts">
 export default {
+  name:"Card",
   data() {
     return {};
   },
-  methods: {}
+  props: {
+    heading: {
+      required: true,
+      type: String,
+      default: ''
+    },
+    data: {
+      required: true,
+      type: Object,
+      default: {}
+    }
+  },
+  //   created(){
+  //       fetch("@/data/courses.json")
+  //       .then(res=>console.log(res.json()))
+  //       .catch(e=>console.log(e))
+  //   },
+  methods: {},
 };
 </script>
 <style lang="scss" scoped>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="about">
-    <h1>دورات ال Vue Js framework</h1>
-    <CardComponent />
+    <CardComponent heading="دورات ال Vue Js framework" :data="course"/>
   </div>
 </template>
 <script lang="ts">
@@ -13,7 +12,27 @@ import CardComponent from "@/components/CardComponent.vue";
     CardComponent
   }
 })
-export default class ContactView extends Vue {}
+export default class ContactView extends Vue {
+  data() {
+    return {
+      course: {
+        id: "PL1FWK-sgJ9ek59ylOuaS8Ggjz9oBVM8Hb",
+        name: "VueJS \u0625\u0633\u0623\u0644\u0646\u064a \u0639\u0646",
+        url:
+          "https://www.youtube.com/watch?v=TQQVt3CbSCw&list=PL1FWK-sgJ9ek59ylOuaS8Ggjz9oBVM8Hb",
+        description: "",
+        thumbnails: "https://i.ytimg.com/vi/TQQVt3CbSCw/sddefault.jpg",
+        statistics: {
+          viewCount: 111,
+          likeCount: 11,
+          commentCount: 7,
+          duration: "00:04:18",
+          lessons: 2
+        }
+      }
+    };
+  }
+}
 </script>
 <style lang="scss" scoped>
 .about {
